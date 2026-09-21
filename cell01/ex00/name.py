fd2476512e5e869867e7e4235ff78319fa77a117
Korakoch <68010017@kmitl.ac.th>
2026-09-22 00:41:09 +0700

    Add cell01 ex00 to ex03

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Korakoch"
+last_name = "Hasap"
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Korakoch"
+last_name = "Hasap"
+whole_name = first_name + " " + last_name
+print(whole_name)
