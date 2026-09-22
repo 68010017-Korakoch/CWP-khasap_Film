0c5d53e1f3fa8ad5e08d0a90268b4668db3ad065
Korakoch <68010017@kmitl.ac.th>
2026-09-22 22:09:01 +0700

    Add cell03 ex00 to ex03

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+
+if len(sys.argv) > 1:
+    print("none")
+    sys.exit()
+
+i = 0
+while i <= 10:
+    j = 0
+    row = f"Table de {i}:"
+    while j <= 10:
+        row += f" {i * j}"
+        j += 1
+    print(row)
+    i += 1
